a9bd51c584c62f70a470a77354e122288c5a22f0
Bhuvan Arnaud <b.arnaud@orisha.com>
2025-05-02 10:05:19 +0200

    feat(server): set global API prefix to 'v1/api'

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,6 +7,8 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.SERVER_PORT || 3000;
+
+  app.setGlobalPrefix("v1/api");
   await app.listen(port);
   process.stdout.write(`Server is running on http://localhost:${port}\n`);
 }
