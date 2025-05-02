8e272ef9b06312afdaaca3d7b38f65cfe3ce75f6
Bhupow <114442972+BhuvanArn@users.noreply.github.com>
2025-05-02 13:30:02 +0200

    Add server api global prefix (#14)

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,39 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.SERVER_PORT || 3000;
+
+  app.setGlobalPrefix("v1/api");
   await app.listen(port);
-  process.stdout.write(`Server is running on http://localhost:${port}\n`);
+  process.stdout.write(`Server is running on ${await app.getUrl()}\n`); // if '::1', it means localhost (IPv6 equivalent of 127.0.0.1)
+
+  process.on("SIGINT", () => {
+    process.stdout.write("\b\bServer is shutting down...\n"); // '\b\b' is used to remove the last two characters from the line (the Ctrl+C characters)
+
+    (async () => {
+      try {
+        await app.close();
+      } catch (error) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        process.stdout.write(`Error shutting down server: ${error.message}\n`);
+      } finally {
+        process.exit(0);
+      }
+    })();
+  });
+
+  process.on("SIGUSR2", () => {
+    process.stdout.write("\b\bServer is restarting...\n");
+
+    (async () => {
+      try {
+        await app.close();
+      } catch (error) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        process.stdout.write(`Error restarting server: ${error.message}\n`);
+      } finally {
+        process.exit(0);
+      }
+    })();
+  });
 }
 bootstrap();
