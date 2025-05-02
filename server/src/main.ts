5e441af10858d1e8bab0ebf4f7eff4c61d37f433
Bhuvan Arnaud <b.arnaud@orisha.com>
2025-05-02 10:13:24 +0200

    fix(server): add graceful shutdown and restart handling

--- server/src/main.ts
+++ server/src/main.ts
@@ -11,5 +11,17 @@ async function bootstrap() {
   app.setGlobalPrefix("v1/api");
   await app.listen(port);
   process.stdout.write(`Server is running on ${await app.getUrl()}\n`); // if '::1', it means localhost (IPv6 equivalent of 127.0.0.1)
+
+  process.on("SIGINT", () => {
+    process.stdout.write("\b\bServer is shutting down...\n"); // '\b\b' is used to remove the last two characters from the line (the Ctrl+C characters)
+    app.close();
+    process.exit(0);
+  });
+
+  process.on("SIGUSR2", () => {
+    process.stdout.write("\b\bServer is restarting...\n");
+    app.close();
+    process.exit(0);
+  });
 }
 bootstrap();
