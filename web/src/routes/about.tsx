a0711829cebfde7068716497c1fa849db014be8d
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2025-05-03 18:49:13 +0200

    add(demo): button demo on about page

--- web/src/routes/about.tsx
+++ web/src/routes/about.tsx
@@ -1,5 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { FaCircleInfo } from 'react-icons/fa6';
+import { FaC, FaCircleInfo } from 'react-icons/fa6';
+
+import { Button } from '@components/atoms/button';
 
 export const Route = createFileRoute('/about')({
   component: About,
@@ -8,9 +10,23 @@ export const Route = createFileRoute('/about')({
 function About() {
   return (
     <div className="p-2">
-      <h3 className="text-primary">About</h3>
-      <FaCircleInfo size={24} color="blue" />
-      <p>A propos</p>
+      <div className="flex items-center gap-2">
+        <h3 className="text-primary">About</h3>
+        <FaCircleInfo size={24} className="fill-primary" />
+      </div>
+      <p className="mb-4">A propos</p>
+      <Button
+        variant="contained"
+        color="primary"
+        roundiness="rounded-full"
+        className="flex gap-4 items-center justify-between"
+        onClick={() => {
+          alert('You clicked the button!');
+        }}
+      >
+        <FaCircleInfo size={24} color="white" />
+        <span className="text-white font-display font-semibold">Click me</span>
+      </Button>
     </div>
   );
 }
