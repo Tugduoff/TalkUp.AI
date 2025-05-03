126c701977712fee670d56f35cba5b98655f014f
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2025-05-03 14:18:33 +0200

    refactor(component): atom component logo refacto to only have the svg logo with color transform in it

---
+++ web/src/components/atoms/logo-svg/index.tsx
@@ -0,0 +1,26 @@
+import { LogoColor } from './types';
+
+interface Props {
+  color?: LogoColor;
+  size?: number;
+}
+
+const LogoSvg = ({ color = 'primary', size = 100 }: Props) => {
+  console.log('LogoSvg fill color:', color);
+
+  const fill = `fill-${color}`;
+
+  return (
+    <svg
+      className={fill}
+      width={size}
+      height={size}
+      viewBox="0 0 78 75"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M0 0.228027H19.7982C19.226 9.5941 26.3244 17.7565 35.9565 18.8082L38.5759 19.0942V37.7639L36.5265 37.7991C30.4232 37.9041 24.4363 36.1753 19.39 32.8508L19.7499 40.8218C19.782 41.532 19.9013 42.236 20.1053 42.9189C22.4482 50.7606 29.9718 56.0774 38.4018 55.8487L38.5759 55.844C48.0075 56.0682 56.1234 49.4323 57.4715 40.3939L57.8638 37.7639H77.1517C77.1517 58.2773 59.8704 74.9067 38.6776 74.9067C17.3728 74.9067 0 58.1893 0 37.5674V0.228027Z" />
+    </svg>
+  );
+};
+
+export default LogoSvg;
