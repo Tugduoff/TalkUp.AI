ae844c8f33c6ffa8ede0ab0f4daec087a588bc9f
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2025-05-03 20:09:24 +0200

    rework(component): remove roundiness and add border transparent

--- web/src/components/atoms/button/variants/contained.tsx
+++ web/src/components/atoms/button/variants/contained.tsx
@@ -1,7 +1,6 @@
 import { ButtonProps as Props } from '../types';
 
 export const ContainedButton = ({
-  roundiness = 'rounded-sm',
   color = 'primary',
   disabled = false,
   loading = false,
@@ -12,7 +11,7 @@ export const ContainedButton = ({
   return (
     <button
       {...props}
-      className={`bg-${color} hover:bg-${color}-hover active:bg-${color}-active py-3 px-5 cursor-pointer ${roundiness} ${!disabled || 'opacity-50 !cursor-not-allowed !bg-disabled hover:!bg-disabled active:!bg-disabled'} ${loading ? 'cursor-wait' : ''} ${props.className || ''}`}
+      className={`border border-transparent bg-${color} hover:bg-${color}-hover active:bg-${color}-active ${color === 'white' ? 'text-black' : 'text-white'} py-3 px-5 cursor-pointer ${!disabled || 'opacity-50 !cursor-not-allowed !bg-disabled hover:!bg-disabled active:!bg-disabled !text-black'} ${loading ? 'cursor-wait' : ''} ${props.className || ''}`}
       onClick={disabled ? undefined : onClick}
       disabled={disabled || loading}
       aria-label="button"
