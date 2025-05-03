a3567ff11ba7c004d5f51874795b2fe50ab97b3f
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2025-05-03 14:21:44 +0200

    add(component): logo column variant

---
+++ web/src/components/molecules/logo/variants/column.tsx
@@ -0,0 +1,17 @@
+import LogoSvg from '@components/atoms/logo-svg';
+import { LogoColor } from '@components/atoms/logo-svg/types';
+
+interface Props {
+  color?: LogoColor;
+}
+
+export const ColumnLogo = ({ color = 'primary' }: Props) => {
+  console.log('ColumnLogo', color);
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-2 h-20">
+      <LogoSvg color={color} size={32} />
+      <h1 className={`text-[20px] leading-[20px] text-${color} uppercase font-display font-extrabold tracking-tight`}>TalkUp</h1>
+    </div>
+  );
+};
