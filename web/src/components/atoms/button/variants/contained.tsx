86f4d57a3de8f30f469c7e3894825e7dc0a00dc0
Tugdual de Reviers <114673495+Tugduoff@users.noreply.github.com>
2025-05-05 16:26:00 +0200

    Create button atomic component (#21)

---
+++ web/src/components/atoms/button/variants/contained.tsx
@@ -0,0 +1,23 @@
+import { ButtonProps as Props } from '../types';
+
+export const ContainedButton = ({
+  color = 'primary',
+  disabled = false,
+  loading = false,
+  children,
+  onClick = () => {},
+  ...props
+}: Props) => {
+  return (
+    <button
+      {...props}
+      className={`border border-transparent bg-${color} hover:bg-${color}-hover active:bg-${color}-active ${color === 'white' ? 'text-black' : 'text-white'} py-3 px-5 cursor-pointer ${disabled ? 'opacity-50 !cursor-not-allowed !bg-disabled hover:!bg-disabled active:!bg-disabled !text-black' : ''} ${loading ? 'cursor-wait' : ''} ${props.className || ''}`}
+      onClick={disabled ? undefined : onClick}
+      disabled={disabled || loading}
+      aria-label="button"
+      aria-disabled={disabled || loading}
+    >
+      {children}
+    </button>
+  );
+};
