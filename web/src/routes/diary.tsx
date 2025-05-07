594be7315bfaf89fd66c62b784ce1d57878e283c
Tugdual de Reviers <derevierst@gmail.com>
2025-05-07 10:18:43 +0200

    add: demo of the base input componnt

--- web/src/routes/diary.tsx
+++ web/src/routes/diary.tsx
@@ -1,14 +1,28 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { useState } from 'react';
+
+import { BaseInput } from '@components/atoms/base-input';
 
 export const Route = createFileRoute('/diary')({
   component: Diary,
 });
 
 function Diary() {
+  const [value, setValue] = useState('');
+
   return (
     <div className="p-2">
       <h3 className="text-primary">Diary</h3>
       <p>Agenda</p>
+      <BaseInput
+        type="text"
+        placeholder="Input"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        disabled={false}
+        readOnly={false}
+        required={true}
+      />
     </div>
   );
 }
