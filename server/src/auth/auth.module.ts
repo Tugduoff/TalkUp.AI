7e85c512eb1d8c1881d4aa41081ed37dc3dd18cb
Bhuvan Arnaud <b.arnaud@orisha.com>
2025-05-07 16:51:59 +0200

    feat(auth): integrate JWT module and enhance user entity imports

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,14 +1,35 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { JwtModule } from "@nestjs/jwt";
 
 import { AuthController } from "./auth.controller";
 
 import { AuthService } from "./auth.service";
 
-import { user } from "src/entities/user.entity";
+import * as dotenv from "dotenv";
+dotenv.config(); // Load environment variables
+
+import {
+  user,
+  user_email,
+  user_password,
+  user_phonenumber,
+} from "src/entities/user.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([user])],
+  imports: [
+    TypeOrmModule.forFeature([
+      user,
+      user_email,
+      user_password,
+      user_phonenumber,
+    ]),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: "7d" }, // Token expiration time
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
