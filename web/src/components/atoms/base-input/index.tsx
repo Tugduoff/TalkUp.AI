72eb2aa9084203cb6613770594d6d252d33b50e1
Tugdual de Reviers <derevierst@gmail.com>
2025-05-07 10:18:29 +0200

    feat: add base input atomic component

---
+++ web/src/components/atoms/base-input/index.tsx
@@ -0,0 +1,34 @@
+interface Props {
+  type?: string;
+  placeholder?: string;
+  value?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  disabled?: boolean;
+  readOnly?: boolean;
+  required?: boolean;
+}
+
+export const BaseInput = ({
+  type = 'text',
+  placeholder = 'Enter text',
+  value = '',
+  onChange = () => {},
+  disabled = false,
+  readOnly = false,
+  required = false,
+  ...props
+}: Props) => {
+  return (
+    <input
+      {...props}
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      disabled={disabled}
+      readOnly={readOnly}
+      required={required}
+      className="px-4 py-2 border border-border-strong placeholder:text rounded-sm font-display font-normal text-sm focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent transition-colors duration-200 ease-in-out disabled:cursor-not-allowed disabled:bg-disabled disabled:opacity-50"
+    />
+  );
+};
