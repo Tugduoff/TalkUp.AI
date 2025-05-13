7c9a263ff701e1489afbee4ec6608dc3a4c19e2a
Bhuvan Arnaud <b.arnaud@orisha.com>
2025-05-13 17:27:02 +0200

    docs(dto): enhance CreateUserDto with Swagger API properties for better documentation

--- server/src/auth/dto/createUser.dto.ts
+++ server/src/auth/dto/createUser.dto.ts
@@ -1,11 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Length, IsPhoneNumber, IsStrongPassword } from "class-validator";
 
 export class CreateUserDto {
   @Length(1, 50)
+  @ApiProperty({
+    description: "The user's name",
+    maxLength: 50,
+    minLength: 1,
+    example: "AdminSys819",
+  })
   username: string;
 
   @Length(1, 50)
   @IsPhoneNumber("FR") // temporary, to be removed when internationalization is implemented
+  @ApiProperty({
+    description:
+      "The user's phone number. It will be used later for verification.",
+    example: "+33123456789",
+  })
   phonenumber: string;
 
   @IsStrongPassword({
@@ -16,5 +28,11 @@ export class CreateUserDto {
     minSymbols: 1,
   })
   @Length(8, 50)
+  @ApiProperty({
+    description:
+      "The user's password. It will be hashed before added to the database.",
+    minLength: 8,
+    example: "Abcdefg1*",
+  })
   password: string;
 }
