778e52b695bac35180017729ae2dc3d7f9e5858b
Bhuvan Arnaud <b.arnaud@orisha.com>
2025-05-16 11:24:53 +0200

    feat(auth): enhance register endpoint with Swagger responses for better API documentation

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,18 +1,32 @@
-import { Body, Controller, HttpCode, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { ValidationPipe } from "@nestjs/common/pipes/validation.pipe";
 import { UsePipes } from "@nestjs/common/decorators/core/use-pipes.decorator";
+
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiTags,
+} from "@nestjs/swagger";
 
 import { CreateUserDto } from "./dto/createUser.dto";
 
 import { AuthService } from "./auth.service";
 
+@ApiTags("Auth")
 @Controller("auth")
 export class AuthController {
   // eslint-disable-next-line no-unused-vars
   constructor(private readonly authService: AuthService) {}
 
   @Post("register")
-  @HttpCode(200)
+  @ApiCreatedResponse({
+    description: "The user has been successfully created.",
+    type: CreateUserDto,
+  })
+  @ApiBadRequestResponse({
+    description:
+      "Badly formatted parameter OR missing parameter OR user already exists.",
+  })
   @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
   async register(@Body() createUserDto: CreateUserDto) {
     // Call the authService to handle the registration logic
