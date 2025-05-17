4c064ddd5031ef9e0b22efc515f320b094ccc9d9
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2025-05-17 19:42:59 +0200

    add: dummy function that validates a phone number format (just dumb for now)

---
+++ web/src/utils/validatePhoneNumber.ts
@@ -0,0 +1,11 @@
+export const validatePhoneNumber = async (phoneNumber: string) => {
+  return new Promise<string | undefined>((resolve) => {
+    setTimeout(() => {
+      resolve(
+        phoneNumber.includes("69")
+          ? undefined
+          : "Invalid phone number"
+      );
+    }, 100);
+  });
+};
