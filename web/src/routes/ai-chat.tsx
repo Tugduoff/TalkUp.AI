ae3306748b2e79a19bd8a275ca8e1c43bf96d4ee
Tugdual de Reviers <derevierst@gmail.com>
2025-05-21 10:36:20 +0200

    add: buttons to simulate actions where toasts would be dispatched

--- web/src/routes/ai-chat.tsx
+++ web/src/routes/ai-chat.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@/components/atoms/button';
 import { createFileRoute } from '@tanstack/react-router';
+import toast from 'react-hot-toast';
 
 export const Route = createFileRoute('/ai-chat')({
   component: AIChat,
@@ -9,6 +11,38 @@ function AIChat() {
     <div className="p-2">
       <h3 className="text-primary">AI Chat</h3>
       <p>Chat IA</p>
+      <div className="flex flex-col gap-2 justify-center items-start mt-4">
+        <Button
+          variant="contained"
+          color="success"
+          className="rounded-md w-40"
+          onClick={() => {
+            toast.success('This is a success message');
+          }}
+        >
+          <span>Success</span>
+        </Button>
+        <Button
+          variant="contained"
+          color="error"
+          className="rounded-md w-40"
+          onClick={() => {
+            toast.error('This is an error message');
+          }}
+        >
+          <span>Error</span>
+        </Button>
+        <Button
+          variant="contained"
+          color="neutral"
+          className="rounded-md w-40"
+          onClick={() => {
+            toast.loading('This is a loading message');
+          }}
+        >
+          <span>Loading</span>
+        </Button>
+      </div>
     </div>
   );
 }
