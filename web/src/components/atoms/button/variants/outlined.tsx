3557d0a6e48b43179990d363cd87b9ad162e749b
Tugdual de Reviers <derevierst@gmail.com>
2025-05-21 11:12:20 +0200

    fix: add loading check also on the onClick func

--- web/src/components/atoms/button/variants/outlined.tsx
+++ web/src/components/atoms/button/variants/outlined.tsx
@@ -12,7 +12,7 @@ export const OutlinedButton = ({
     <button
       {...props}
       className={`border-${color} border text-${color} ${color === 'white' || color === 'black' ? '!text-black !border-black' : ''} py-3 px-5 cursor-pointer ${disabled ? 'opacity-50 !cursor-not-allowed !border-disabled !bg-transparent !text-disabled' : ''} ${loading ? 'cursor-wait' : `hover:border-${color}-hover active:border-${color}-active hover:text-${color}-hover active:text-${color}-active hover:bg-${color}-weaker active:bg-${color}-weak`} ${props.className || ''}`}
-      onClick={disabled ? undefined : onClick}
+      onClick={disabled || loading ? undefined : onClick}
       disabled={disabled || loading}
       aria-label="button"
       aria-disabled={disabled || loading}
--- web/src/components/atoms/button/variants/text.tsx
+++ web/src/components/atoms/button/variants/text.tsx
@@ -12,7 +12,7 @@ export const TextButton = ({
     <button
       {...props}
       className={`border border-transparent text-${color} ${color === 'white' || color === 'black' ? '!text-black' : ''} py-3 px-5 cursor-pointer ${disabled ? 'opacity-50 !cursor-not-allowed !bg-transparent !text-disabled' : ''} ${loading ? 'cursor-wait' : `hover:text-${color}-hover active:text-${color}-active hover:bg-${color}-weaker active:bg-${color}-weak`} ${props.className || ''}`}
-      onClick={disabled ? undefined : onClick}
+      onClick={disabled || loading ? undefined : onClick}
       disabled={disabled || loading}
       aria-label="button"
       aria-disabled={disabled || loading}
