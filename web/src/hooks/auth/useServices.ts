d1d1728dc63ac86cd226452bde7669b99fb9a917
Tugdual de Reviers <114673495+Tugduoff@users.noreply.github.com>
2025-05-22 14:50:03 +0200

    Add jsdoc documentation in web files (#33)

--- web/src/hooks/auth/useServices.ts
+++ web/src/hooks/auth/useServices.ts
@@ -4,6 +4,28 @@ import toast from 'react-hot-toast';
 
 const authService = new AuthService();
 
+/**
+ * Custom hook for user registration functionality.
+ *
+ * This hook uses the React Query's useMutation to handle the registration process.
+ * It takes username, phoneNumber, and password as inputs, sends them to the authentication service,
+ * and stores the returned access token in localStorage.
+ *
+ * @returns A mutation object that can be used to trigger the registration process
+ * and monitor its state.
+ *
+ * @example
+ * const registerMutation = usePostRegister();
+ *
+ * // Later in a component:
+ * const handleRegister = () => {
+ *   registerMutation.mutate({
+ *     username: "user123",
+ *     phoneNumber: "+1234567890",
+ *     password: "securePassword"
+ *   });
+ * };
+ */
 export const usePostRegister = () => {
   return useMutation({
     mutationFn: async ({
