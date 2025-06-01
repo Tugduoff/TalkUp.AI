add9cdd362abcf2b7cd33b98fdd1db7da68c993b
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2025-06-01 18:55:27 +0200

    feat: add enumMcs and notifications classes to the microservices section

---
+++ ai/core/microservices/enumMcs.py
@@ -0,0 +1,15 @@
+##
+## Talkup Project, 2025
+## TalkUp.AI
+## File description:
+## This module defines the enumeration for microservices.
+##
+
+from enum import Enum
+
+class MicroservicesNames(Enum):
+    BA = 0
+    EA = 1
+    STT = 2
+    TTS = 3
+    VA = 4
---
+++ ai/core/microservices/notifications.py
@@ -0,0 +1,30 @@
+##
+## Talkup Project, 2025
+## TalkUp.AI
+## File description:
+## Handles notifications for the TalkUp AI microservices.
+##
+
+from enumMcs import MicroservicesNames
+
+class Notifications():
+    def __init__(self):
+        """
+        Class constructor
+        """
+        pass
+
+    def send_notification(self, service: MicroservicesNames, msg: str) -> None:
+        """
+        Send a notification to the console.
+        """
+        print(f"[{service.name}] 💡 {msg}", end='\n')
+
+    def send_start_notification(self, service: MicroservicesNames, successful: bool)-> None:
+        """
+        Send a start notification to the console with the status of the service.
+        """
+        if (successful):
+            print (f"[SERVICES] 💡 {service.name} started successfully!")
+        else:
+            print (f"[SERVICES] 💡 Failed to start {service.name}")
