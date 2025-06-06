d6ababd28799ea8dc8d5b1eaf6aacd7260287ba4
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2025-06-06 23:50:56 +0200

    feat: add base url in playwright config to make tests more maintainable

--- web/tests/example.spec.ts
+++ web/tests/example.spec.ts
@@ -1,13 +1,13 @@
 import { expect, test } from '@playwright/test';
 
 test('Go to About page', async ({ page }) => {
-  await page.goto('http://localhost:8080/');
+  await page.goto('/');
   await expect(page.locator('h3')).toContainText('Home');
   await page.getByText('Page vitrine').click();
   await page.getByText('Change Logo color by clicking').click();
   await page.getByRole('link', { name: 'About' }).click();
   await page.waitForLoadState('networkidle');
-  await expect(page).toHaveURL('http://localhost:8080/about');
+  await expect(page).toHaveURL('/about');
   await expect(page.getByRole('heading', { name: 'About' })).toContainText(
     'About',
   );
