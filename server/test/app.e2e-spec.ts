8c075f173a70b0aadf528d60426cdf1ca8d21fd6
eregine <114678670+eregine@users.noreply.github.com>
2025-07-15 12:33:41 +0200

    Add new test for the route updatePassword to fit the new test coverage

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -22,4 +22,44 @@ describe("AppController (e2e)", () => {
       .expect(200)
       .expect("Hello World!");
   });
+  it("/updatePassword (PUT)", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    await request(app.getHttpServer())
+      .post("/register")
+      .send({
+        username: "name",
+        phoneNumber: "0600000000",
+        password: "oldPassword",
+      })
+      .expect(201);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const res = await request(app.getHttpServer())
+      .put("/updatePassword")
+      .send({
+        phoneNumber: "0600000000",
+        newPassword: "newSecurePassword123",
+      })
+      .expect(200);
+    expect(res.body as boolean).toEqual(true);
+  });
+  it("/updatePassword (PUT)", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const res = await request(app.getHttpServer())
+      .put("/updatePassword")
+      .send({
+        phoneNumber: "0999999999", // non-existent number
+        newPassword: "somePassword",
+      })
+      .expect(401);
+    expect((res.body as { message: string }).message).toBe(
+      "There is no user with that phone number",
+    );
+  });
+  it("/updatePassword (PUT)", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    await request(app.getHttpServer())
+      .put("/updatePassword")
+      .send({}) // no data send
+      .expect(400);
+  });
 });
