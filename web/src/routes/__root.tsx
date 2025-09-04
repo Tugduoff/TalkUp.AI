c349362a193655463618ef23721859a9b40d4161
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2025-09-04 14:51:41 +0200

    rework: use Navbar instead of list of links

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,34 +1,15 @@
-import { Link, Outlet, createRootRoute } from '@tanstack/react-router';
+import NavBar from '@/components/organisms/navbar';
+import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
-
-const links = [
-  { to: '/', label: 'Home' },
-  { to: '/about', label: 'About' },
-  { to: '/login', label: 'Login' },
-  { to: '/signup', label: 'SignUp' },
-  { to: '/profile', label: 'Profile' },
-  { to: '/dashboard', label: 'Dashboard' },
-  { to: '/simulations', label: 'Simulations' },
-  { to: '/cv-analysis', label: 'CV Analysis' },
-  { to: '/ai-chat', label: 'AI Chat' },
-  { to: '/diary', label: 'Diary' },
-  { to: '/notes', label: 'Notes' },
-  { to: '/progression', label: 'Progression' },
-];
 
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <div className="flex justify-around gap-2 p-2">
-        {links.map(({ to, label }) => (
-          <Link key={to} to={to} className="[&.active]:font-bold">
-            {label}
-          </Link>
-        ))}
-      </div>
-      <hr />
-      <Outlet />
+    <div className="grid grid-cols-[256px_1fr] h-screen">
+      <NavBar />
+      <main className="overflow-auto">
+        <Outlet />
+      </main>
       <TanStackRouterDevtools />
-    </>
+    </div>
   ),
 });
