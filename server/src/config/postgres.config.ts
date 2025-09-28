c478597188a150d97ea8c67080d818a61acb565e
Tugduoff <tugdual.de-reviers@epitech.eu>
2025-09-28 21:04:03 +0200

    fix(format): in postgres.config.ts

--- server/src/config/postgres.config.ts
+++ server/src/config/postgres.config.ts
@@ -1,7 +1,9 @@
 import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
 
 export default (): PostgresConnectionOptions => {
-  const port = process.env.POSTGRES_PORT ? parseInt(process.env.POSTGRES_PORT) : 5432;
+  const port = process.env.POSTGRES_PORT
+    ? parseInt(process.env.POSTGRES_PORT)
+    : 5432;
   const host =
     process.env.DOCKER_RUN === "true"
       ? process.env.POSTGRES_DOCKER_HOST
