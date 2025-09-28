9e4a38d31474c30218bbaae3364a7fab18c38cc5
Tugduoff <tugdual.de-reviers@epitech.eu>
2025-09-28 23:28:23 +0200

    feat: add routes auth guard

--- web/src/routes/dashboard.tsx
+++ web/src/routes/dashboard.tsx
@@ -1,6 +1,8 @@
+import { createAuthGuard } from '@/utils/auth.guards';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/dashboard')({
+  beforeLoad: createAuthGuard('/dashboard'),
   component: Dashboard,
 });
 
--- web/src/routes/simulations.tsx
+++ web/src/routes/simulations.tsx
@@ -1,6 +1,8 @@
+import { createAuthGuard } from '@/utils/auth.guards';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/simulations')({
+  beforeLoad: createAuthGuard('/simulations'),
   component: Simulations,
 });
 
