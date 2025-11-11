52abc3ec3cf09a43fc612bbae38e127fbe1e55eb
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2025-11-11 16:30:42 +0900

    chore: add function prototype in the WebsocketManager.hpp

--- ai/core/server/inc/network/WebsocketManager.hpp
+++ ai/core/server/inc/network/WebsocketManager.hpp
@@ -8,6 +8,8 @@
 
 #pragma once
 
+#include <string>
+#include <cstdint>
 #include <nlohmann/json.hpp>
 #include <crow.h>
 
@@ -36,6 +38,18 @@ namespace talkup_network {
              * @param conn The WebSocket connection object.
              */
             void connection_type_manager(nlohmann::json &j, crow::websocket::connection& conn);
+            /**
+             * @brief With the given parameters, it will set the respond JSON format and return it.
+             *
+             * @param type The type of the message.
+             * @param key The key associated with the message.
+             * @param stream_id The stream ID associated with the message.
+             * @param format The format of the message.
+             * @param timestamp The timestamp of the message.
+             * @param data The data of the message.
+             */
+            nlohmann::json set_respond_json_format(const std::string& type, const std::string& key,
+                const std::string& stream_id, const std::string& format, int64_t timestamp, const std::string& data) const;
 
         protected:
         private:
