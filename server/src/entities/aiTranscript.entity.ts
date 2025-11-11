edefefc6b35494fb0aa168f9bdd7f9e9159dbda6
BhuvanArn <bhuvanarnaud@gmail.com>
2025-11-12 00:55:38 +0800

    feat(entity): add ai_transcript entity with fields and UUID generation

---
+++ server/src/entities/aiTranscript.entity.ts
@@ -0,0 +1,43 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  JoinColumn,
+  BeforeInsert,
+  ManyToOne,
+} from "typeorm";
+import { uuidv7 } from "uuidv7";
+
+import { AiTranscriptStated } from "@common/enums/AiTranscriptStated";
+
+import { ai_interview } from "./aiInterview.entity";
+
+@Entity()
+export class ai_transcript {
+  @PrimaryGeneratedColumn("uuid")
+  transcript_id: string;
+
+  @ManyToOne(() => ai_interview, (interview) => interview.interview_id, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: "interview_id" })
+  interview_id: string;
+
+  @Column({ type: "text" })
+  content: string;
+
+  @Column({
+    type: "enum",
+    enum: AiTranscriptStated
+  })
+  who_stated: AiTranscriptStated;
+
+  @CreateDateColumn({ type: "timestamp with time zone", default: () => "CURRENT_TIMESTAMP" })
+  inserted_at: string;
+
+  // ------ UUID manual generation to ensure V7 format ------ //
+
+  @BeforeInsert()
+  generateUUIDv7() {
+      if (!this.transcript_id) this.transcript_id = uuidv7();
+  }
+}
