4d0b2e0f63ed4d7ac41051b0fa25728cefa69bcb
BhuvanArn <bhuvanarnaud@gmail.com>
2025-11-16 06:33:31 +0800

    feat(module): create AgendaModule with imports, controllers, and providers for event management

---
+++ server/src/modules/agenda/agenda.module.ts
@@ -0,0 +1,25 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { JwtModule } from "@nestjs/jwt";
+
+import { agenda_event } from "@entities/agenda.entity";
+import { AccessTokenGuard } from "@common/guards/accessToken.guard";
+import { user } from "@entities/user.entity";
+
+import { AgendaService } from "./agenda.service";
+import { AgendaController } from "./agenda.controller";
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([agenda_event, user]),
+    JwtModule.register({
+        global: true,
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: "7d" },
+    }),
+],
+  controllers: [AgendaController],
+  providers: [AgendaService, AccessTokenGuard],
+  exports: [AgendaService],
+})
+export class AgendaModule {}
